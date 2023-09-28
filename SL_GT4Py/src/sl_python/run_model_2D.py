9ef27dbec206388f45101e65beed7b4531b21651
Loic Maurin <maurin.loic.ac@gmail.com>
2023-09-28 10:52:05 +0200

    Boundaries processing refactored in a single function

--- SL_GT4Py/src/sl_python/run_model_2D.py
+++ SL_GT4Py/src/sl_python/run_model_2D.py
@@ -2,7 +2,9 @@ from typing import Tuple
 import numpy as np
 
 
-def interpolate_linear_2d(lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray):
+def interpolate_linear_2d(
+    lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray
+):
     """Interpolate sequentially on x axis and on y axis
 
     Args:
@@ -15,14 +17,16 @@ def interpolate_linear_2d(lx: np.float64, ly: np.float64, ii: int, jj: int, fiel
     """
     p0 = lambda l: 1 - l
     p1 = lambda l: l
-    
+
     px = np.array([p0(lx), p1(lx)])
     py = np.array([p0(ly), p1(ly)])
-    
-    return field[ii: ii + 2, jj: jj + 2] * px * py.T
+
+    return field[ii : ii + 2, jj : jj + 2] * px * py.T
 
 
-def interpolate_cubic_2d(lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray):
+def interpolate_cubic_2d(
+    lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray
+):
     """Interpolate sequentially on x axis and on y axis.
 
     Args:
@@ -41,11 +45,67 @@ def interpolate_cubic_2d(lx: np.float64, ly: np.float64, ii: int, jj: int, field
     p0 = lambda l: (1 - l**2) * (1 - l / 2)
     p1 = lambda l: 0.5 * l * (l + 1) * (l - 2)
     p2 = lambda l: 0.5 * l * (l**2 - 1) / 3
-    
+
     px = np.array([p_1(lx), p0(lx), p1(lx), p2(lx)])
     py = np.array([p_1(ly), p0(ly), p1(ly), p2(ly)])
-    
-    return field[ii - 1: ii + 3, jj - 1: jj + 3] * px * py.T
+
+    return field[ii - 1 : ii + 3, jj - 1 : jj + 3] * px * py.T
+
+
+def boundaries(
+    bc_kind: int, points: np.ndarray, indices: np.ndarray, l: np.ndarray , xmin: np.float, xmax: np.float, nx: int
+):
+    """Apply boundary conditions on field.
+
+    Args:
+        bc_kind (int): _description_
+        field (np.ndarray): _description_
+        min (np.float): _description_
+        max (np.float): _description_
+        nx (int): _description_
+    """
+    left_exceed = (points > xmax).astype(np.int8)
+    right_exceed = (points < xmin).astype(np.int8)
+
+    left_recovering = xmin + points - xmax
+    right_recovering = xmax + points - xmin
+
+    # Periodic boundaries
+    if bool(bc_kind):
+        points = (
+            points * (1 - right_exceed) * (1 - left_exceed)
+            + right_exceed * left_recovering
+            + left_exceed * right_recovering
+        )
+
+        indices = (
+            ii * (1 - right_exceed) * (1 - left_exceed)
+            + right_exceed * np.floor(left_recovering / dx)
+            + left_exceed * np.floor(right_recovering / dx)
+        )
+
+        l = (
+            l * (1 - right_exceed) * (1 - left_exceed)
+            + right_exceed * (left_recovering / dx - np.floor(left_recovering / dx))
+            + left_exceed * (right_recovering / dx)
+            - np.floor(right_recovering / dx)
+        )
+
+    # Fixed boundaries
+    else:
+        points = (
+            points * (1 - right_exceed) * (1 - left_exceed)
+            + xmin * left_exceed
+            + xmax * right_exceed
+        )
+
+        indices = (
+            indices * (1 - right_exceed) * (1 - left_exceed)
+            + 0 * left_exceed
+            + (nx - 1) * right_exceed
+        )
+
+        l = l * (1 - right_exceed) * (1 - left_exceed)
 
 
 def lagrangian_search(
@@ -58,9 +118,7 @@ def lagrangian_search(
     dt: np.float64,
     nx: int,
     ny: int,
-    nsiter: int,
-    LNESC: bool,
-    LSETTLS: bool
+    nsiter: int = 10,
 ) -> Tuple[np.ndarray]:
     """Research departure point for a given grid and velocity field.
     Terminates on nsiter iterations.
@@ -76,12 +134,12 @@ def lagrangian_search(
 
     vx_e, vy_e = v_e[0], v_e[1]
     vx, vy = v[0], v[1]
-    
+
     # Array declaration
     vx_d = np.zeros((nx, ny))
     vy_d = np.zeros((nx, ny))
-    
-    vx_d_p = np.zeros((nx, ny))+4
+
+    vx_d_p = np.zeros((nx, ny)) + 4
     vy_d_p = np.zeros((nx, ny))
 
     ################ l = 1 ##############
@@ -98,148 +156,72 @@ def lagrangian_search(
     ii = I - np.floor(disp_x / dx)
     jj = J - np.floor(disp_y / dy)
 
-    # Lambda 
+    # Lambda
     lx = dx * (disp_x / dx - np.floor(disp_x / dx))
     ly = dy * (disp_y / dy - np.floor(disp_y / dy))
-    
-    
+
+    # Boundary processing
+    boundaries(bcx_kind, x_dep, ii, lx, xmin, xmax, nx)
+    boundaries(bcy_kind, y_dep, jj, ly, ymin, ymax, ny)
+
     for i in range(nx):
         for j in range(ny):
-            vx_d[i, j] = interpolate_linear_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vx)
-            vy_d[i, j] = interpolate_linear_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vy)
-            
-            vx_d_p[i, j] = interpolate_linear_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vx_p)
-            vy_d_p[i, j] = interpolate_linear_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vy_p)
-    
+            vx_d[i, j] = interpolate_cubic_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vx)
+            vy_d[i, j] = interpolate_cubic_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vy)
+
+            vx_d_p[i, j] = interpolate_cubic_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vx_p)
+            vy_d_p[i, j] = interpolate_cubic_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=vy_p)
+
     ################ l > 1 ##################
     for l in range(1, nsiter):
-            
         disp_x = 0.5 * dt * (vx_e + vx_d)
-        x_dep = x - disp_x 
-        
+        x_dep = x - disp_x
+
         disp_y = 0.5 * dt * (vy_e + vy_d)
-        y_dep = y - disp_y 
-        
+        y_dep = y - disp_y
+
         lx = disp_x / dx - np.floor(disp_x / dx)
         ly = disp_y / dy - np.floor(disp_y / dy)
-        
+
         ii = I - np.floor(disp_x / dx)
         jj = J - np.floor(disp_y / dy)
+        
+        boundaries(bcx_kind, x_dep, ii, lx, xmin, xmax, nx)
+        boundaries(bcy_kind, y_dep, jj, ly, ymin, ymax, ny)
 
-        left_exceed = (x_dep > xmax).astype(np.int8)
-        right_exceed = (x_dep < xmin).astype(np.int8)
-        
-        left_recovering = xmin + x_dep - xmax
-        right_recovering = xmax + x_dep - xmin
-        
-        top_exceed = (y_dep > ymax).astype(np.int8)
-        bottom_exceed = (y_dep < ymin).astype(np.int8)
-        
-        bottom_recovering = ymin + y_dep - ymax
-        top_recovering = ymax + y_dep - ymin
-        
-        # Periodic x boundaries
-        if bool(bcx_kind):
-            x_dep = (
-                x_dep * (1 - right_exceed)* (1 - left_exceed)
-                +  right_exceed * left_recovering
-                +  left_exceed * right_recovering
-                )
-            
-            ii = (
-                ii * (1 - right_exceed)* (1 - left_exceed)
-                +  right_exceed * np.floor(left_recovering / dx) 
-                +  left_exceed * np.floor(right_recovering / dx)
-                )
-            
-            lx = (
-                lx * (1 - right_exceed)* (1 - left_exceed)
-                +  right_exceed * (left_recovering / dx - np.floor(left_recovering / dx))
-                +  left_exceed * (right_recovering / dx) - np.floor(right_recovering / dx)
-            )
-           
-        # Fixed x boundaries
-        else:
-            x_dep = (
-                x_dep * (1 - right_exceed) * (1 - left_exceed)
-                +  xmin * left_exceed
-                +  xmax  * right_exceed
-                )    
-            
-            ii = (
-                ii * (1 - right_exceed) * (1 - left_exceed)
-                +  0 * left_exceed
-                +  (nx - 1)  * right_exceed
-                ) 
-            
-            lx = lx * (1 - right_exceed) * (1 - left_exceed)      
-        
-        # Periodic y boundaries
-        if bool(bcy_kind):
-            y_dep = (
-                y_dep * (1 - top_exceed)* (1 - bottom_exceed)
-                +  top_exceed * bottom_recovering
-                +  bottom_exceed * top_recovering
-                )
-            
-            jj = (
-                jj * (1 - top_exceed)* (1 - bottom_exceed)
-                +  top_exceed * np.floor(bottom_recovering / dy) 
-                +  bottom_exceed * np.floor(top_recovering / dy)
-                )
-            
-            ly = (
-                ly * (1 - top_exceed)* (1 - bottom_exceed)
-                +  top_exceed * (bottom_recovering / dy - np.floor(bottom_recovering / dy))
-                +  bottom_exceed * (top_recovering / dy) - np.floor(top_recovering / dy)
-                )            
-           
-        # Fixed y boundaries 
-        else:
-            y_dep = (
-                y_dep * (1 - top_exceed) * (1 - bottom_exceed)
-                +  ymin * top_exceed
-                +  ymax * bottom_exceed
-                )  
-            
-            jj = (
-                jj * (1 - top_exceed) * (1 - bottom_exceed)
-                +  0 * bottom_exceed
-                +  (ny - 1)  * top_exceed
-                ) 
-            
-            ly = ly * (1 - top_exceed) * (1 - bottom_exceed)  
-                    
-        
         ####### Interpolation for fields ########
         for i in range(nx):
             for j in range(ny):
-                vx_e[i, j] = interpolate_cubic_2d(lx[i, j], ly[i, j], ii[i, j], jj[i, j], vx_e)
-                vy_e[i, j] = interpolate_cubic_2d(lx[i, j], ly[i, j], ii[i, j], jj[i, j], vy_e)
-                
-                vx[i, j] = interpolate_cubic_2d(lx[i, j], ly[i, j], ii[i, j], jj[i, j], vx_d)
-                vy[i, j] = interpolate_cubic_2d(lx[i, j], ly[i, j], ii[i, j], jj[i, j], vy_d)
-                
-    return x_dep, y_dep, lx, ly, ii, jj    
-    
-    
+                vx_e[i, j] = interpolate_cubic_2d(
+                    lx[i, j], ly[i, j], ii[i, j], jj[i, j], vx_e
+                )
+                vy_e[i, j] = interpolate_cubic_2d(
+                    lx[i, j], ly[i, j], ii[i, j], jj[i, j], vy_e
+                )
+
+                vx[i, j] = interpolate_cubic_2d(
+                    lx[i, j], ly[i, j], ii[i, j], jj[i, j], vx_d
+                )
+                vy[i, j] = interpolate_cubic_2d(
+                    lx[i, j], ly[i, j], ii[i, j], jj[i, j], vy_d
+                )
+
+    return x_dep, y_dep, lx, ly, ii, jj
+
 
 if __name__ == "__main__":
-    
     # Init option
     LSETTLS = True
     LNESC = False
-    
+
     # Grid
     xmin, xmax = 0, 100
     ymin, ymax = 0, 100
-    zmin, zmax = 0, 100
-    nx, ny, nz = 100, 100, 100
+    nx, ny = 100, 100, 100
 
     # spacings
     dx = (xmax - xmin) / nx
     dy = (ymax - ymin) / ny
-    dz = (zmax - zmin) / nz
 
     # Boundaries
     # 1 : PERIODIC
@@ -251,15 +233,14 @@ if __name__ == "__main__":
     # Spacing
     xc = np.linspace(xmin, xmax, nx)
     yc = np.linspace(ymin, ymax, ny)
-    zc = np.linspace(zmin, zmax, nz)
     x, y = np.meshgrid(xc, yc)
-    
+
     # Horizontal indexes
     i_indices = np.arange(0, nx)
     j_indices = np.arange(0, ny)
 
     I, J = np.meshgrid((i_indices, j_indices))
-    
+
     # Initialisation simple
     # Vent uniforme
     U, V = 20, 0, 0
@@ -271,30 +252,30 @@ if __name__ == "__main__":
 
     # TODO: Revoir l'initilisation
     vx, vy = U * np.ones((nx, ny)), V * np.ones((nx, ny))
+
+    # Champs de vent à t - dt
     vx_p, vy_p = vx.copy(), vy.copy()
-    
+
     # Champs de vent à t + dt
     vx_e, vy_e = np.zeros((nx, ny))
-    
 
-    psi = np.zeros((nx, ny, nz))
+    psi = np.zeros((nx, ny))
     psib = psi.copy()
-    
+
     ########## Premier pas ###########
     # j_iter = 0
     if LNESC:
         vx_e = vx.copy()
-        
+
     elif LSETTLS:
         vx_e = vx.copy()
-        vx = 2 * vx - vx_p    
-        
-    ######### j_iter > 0 ##########   
+        vx = 2 * vx - vx_p
+
+    ######### j_iter > 0 ##########
     for j_iter in range(1, NITMP):
-        
         # Backup psi(t - dt)
         psi_b = psi.copy()
-    
+
         # Recherche semi lag
         x_dep, y_dep, lx, ly, ii, jj = lagrangian_search(
             x=x, y=y, I=I, J=J, v_e=(vx_e, vy_e), v=(vx, vy)
@@ -302,6 +283,6 @@ if __name__ == "__main__":
 
         for i in range(nx):
             for j in range(ny):
-                psi[i, j] = interpolate_cubic_2d(lx=lx, ly=ly, ii=ii, jj=jj, field=psi_b)
-                
-                # Interpolation vitesse ?
+                psi[i, j] = interpolate_cubic_2d(
+                    lx=lx, ly=ly, ii=ii, jj=jj, field=psi_b
+                )
