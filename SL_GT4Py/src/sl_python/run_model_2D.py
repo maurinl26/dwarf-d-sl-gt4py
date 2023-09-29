734bd923ac52b9ca94557c6000e9079b7f07026d
Loic Maurin <maurin.loic.ac@gmail.com>
2023-09-29 10:15:47 +0200

    Interpolation performed on padded array

--- SL_GT4Py/src/sl_python/run_model_2D.py
+++ SL_GT4Py/src/sl_python/run_model_2D.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 import numpy as np
 
+
 def interpolate_linear_2d(
     lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray
 ):
@@ -25,19 +26,32 @@ def interpolate_linear_2d(
 
 def interpolate_cubic_2d(
     lx: np.float64, ly: np.float64, ii: int, jj: int, field: np.ndarray
-):
+) -> np.float64:
     """Interpolate sequentially on x axis and on y axis.
 
     Args:
         lx (np.float64): _description_
         ly (np.float64): _description_
-        ii (int): _description_
+        ii (int): _descriptionsaliur
         jj (int): _description_
         field (np.ndarray): _description_
 
     Returns:
         _type_: _description_
     """
+    
+    # Padding on interpolation field
+    # Fixed
+    if bcx_kind == 0:
+        padded_field = np.pad(field, (1, 2), "edge")
+        ii += 1
+        jj += 1
+        
+    # Periodic
+    else:
+        padded_field = np.pad(field, (1, 2), "wrap")
+        ii += 1
+        jj += 1
 
     # Polynomes de lagrange d'ordre 3
     p_1 = lambda l: 0.5 * l * (l - 1) * (2 - l) / 3
@@ -46,9 +60,9 @@ def interpolate_cubic_2d(
     p2 = lambda l: 0.5 * l * (l**2 - 1) / 3
 
     px = np.array([p_1(lx), p0(lx), p1(lx), p2(lx)])
-    py = np.array([p_1(ly), p0(ly), p1(ly), p2(ly)])
-
-    return field[ii - 1 : ii + 3, jj - 1 : jj + 3] * px * py.T
+    py = np.array([p_1(ly), p0(ly), p1(ly), p2(ly)])        
+    
+    return padded_field[ii - 1 : ii + 3, jj - 1 : jj + 3] * px * py.T
 
 
 def boundaries(
@@ -98,21 +112,21 @@ def boundaries(
 
     # Fixed boundaries
     else:
-        point = (
-            point * (1 - right_exceed) * (1 - left_exceed)
+        points = (
+            points * (1 - right_exceed) * (1 - left_exceed)
             + xmin * left_exceed
             + xmax * right_exceed
         )
 
-        indice = (
-            indice * (1 - right_exceed) * (1 - left_exceed)
+        indices = (
+            indices * (1 - right_exceed) * (1 - left_exceed)
             + 0 * left_exceed
             + (nx - 1) * right_exceed
         )
 
         l = l * (1 - right_exceed) * (1 - left_exceed)
-
-    return indices, l
+        
+    return indices.astype(np.int8), l
 
 
 def lagrangian_search(
@@ -141,15 +155,14 @@ def lagrangian_search(
         np.ndarray: departure point
     """
 
-    # Array declaration    
+    # Array declaration
     ################ l > 1 ##################
     for l in range(0, nsiter):
-        
         if l == 0:
             disp_x = dt * vx
             disp_y = dt * vy
-            
-        else: 
+
+        else:
             disp_x = 0.5 * dt * (vx_e + vx)
             disp_y = 0.5 * dt * (vy_e + vy)
 
@@ -168,7 +181,7 @@ def lagrangian_search(
         ####### Interpolation for fields ########
         for i in range(nx):
             for j in range(ny):
-                
+                                 
                 # interpolation en r_d(l) -> i_d(l), j_d(l)
                 vx_e[i, j] = interpolate_cubic_2d(
                     lx[i, j], ly[i, j], i_d[i, j], j_d[i, j], vx
@@ -176,7 +189,7 @@ def lagrangian_search(
                 vy_e[i, j] = interpolate_cubic_2d(
                     lx[i, j], ly[i, j], i_d[i, j], j_d[i, j], vy
                 )
-                
+
                 # interpolation en r_d(l) -> i_d(l), j_d(l)
                 vx[i, j] = interpolate_cubic_2d(
                     lx[i, j], ly[i, j], i_d[i, j], j_d[i, j], vx_p
@@ -222,17 +235,22 @@ def sl_xy(
     vy: np.ndarray,
     vx_e: np.ndarray,
     vy_e: np.ndarray,
+    tracer: np.ndarray,
     tracer_e: np.ndarray,
+    dt: np.float64,
+    nx: int,
+    ny: int
 ):
     # Recherche semi lag
     lx_d, ly_d, i_d, j_d, vx_e, vy_e = lagrangian_search(
-        x=x, y=y, I=I, J=J, vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy
+        x=x, y=y, I=I, J=J, vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy, dt=dt, nx=nx, ny=ny
     )
 
     # Interpolate
     for i in range(nx):
         for j in range(ny):
             # Interpolate tracer in T(r, t) = T(r_d, t - dt)
+            
             tracer_e[i, j] = interpolate_cubic_2d(
                 lx=lx_d, ly=ly_d, ii=i_d, jj=j_d, field=tracer
             )
@@ -244,12 +262,11 @@ if __name__ == "__main__":
     # Init option
     LSETTLS = True
     LNESC = not LSETTLS  # Pour info
-    
+
     # Iterations
     NSITER = 5  # Semi lagrangian step
     NITMP = 20
     dt = 10
-
 
     # Grid
     xmin, xmax = 0, 100
@@ -272,13 +289,13 @@ if __name__ == "__main__":
     x, y = np.meshgrid(xc, yc)
 
     # Horizontal indexes
-    i_indices = np.arange(0, nx)
-    j_indices = np.arange(0, ny)
-    I, J = np.meshgrid((i_indices, j_indices))
+    i_indices = np.arange(0, nx).astype(np.int8)
+    j_indices = np.arange(0, ny).astype(np.int8)
+    I, J = np.meshgrid(i_indices, j_indices)
 
     # Initialisation simple
     # Vent uniforme
-    U, V = 20, 0, 0
+    U, V = 20, 0
 
     ############## Declaration des champs #############
     vx, vy = (U * np.ones((nx, ny)), V * np.ones((nx, ny)))
@@ -294,7 +311,6 @@ if __name__ == "__main__":
     tracer = T * np.ones((nx, ny))
     tracer_e = np.zeros((nx, ny))
 
-
     ############# Advection
     ########## Premier pas ###########
     # jstep = 0
@@ -306,11 +322,24 @@ if __name__ == "__main__":
 
     ######### j_iter > 0 ##########
     for jstep in range(1, NITMP):
-        
         # Copie des champs
         tracer = tracer_e.copy()
         vx = vx_e.copy()
         vy = vy_e.copy()
-        
+
         # Estimations
-        vx_e, vy_e, tracer_e = sl_xy(I=I, J=J, x=x, y=y, vx=vx, vy=vy, vx_e=vx_e, vy_e=vy_e, tracer=tracer)
+        vx_e, vy_e, tracer_e = sl_xy(
+            I=I,
+            J=J,
+            x=x,
+            y=y,
+            vx=vx,
+            vy=vy,
+            vx_e=vx_e,
+            vy_e=vy_e,
+            tracer=tracer,
+            tracer_e=tracer_e,
+            dt=dt,
+            nx=nx,
+            ny=ny
+        )
