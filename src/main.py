248338d6167ae22188f44531127187f8014c9bd3
Loic Maurin <maurin.loic.ac@gmail.com>
2023-10-03 14:31:42 +0200

    Grid size changed

--- src/main.py
+++ src/main.py
@@ -11,15 +11,15 @@ sys.path.append("/home/maurinl/SL_GT4Py/SL8GT4Py/src")
 from sl_python.sl_2D import sl_init, sl_xy, backup
 from sl_python.plot import plot_2D_scalar_field
 
+
 def main(lsettls: bool, nsiter: int, nitmp: int):
-    
-    lnesc = not lsettls    
+    lnesc = not lsettls
     dt = 1
 
     # Grid
-    xmin, xmax = 0, 100
-    ymin, ymax = 0, 100
-    nx, ny = 100, 100
+    xmin, xmax = 0, 123
+    ymin, ymax = 0, 123
+    nx, ny = 123, 123
 
     # spacings
     dx = (xmax - xmin) / nx
@@ -28,8 +28,8 @@ def main(lsettls: bool, nsiter: int, nitmp: int):
     # Boundaries
     # 1 : PERIODIC
     # 0 : FIXED
-    bcx_kind = 0
-    bcy_kind = 0
+    bcx_kind = 1
+    bcy_kind = 1
 
     # Spacing
     xc = np.linspace(xmin, xmax, nx)
@@ -37,13 +37,13 @@ def main(lsettls: bool, nsiter: int, nitmp: int):
     xcr, ycr = np.meshgrid(xc, yc)
 
     # Horizontal indexes
-    i_indices = np.arange(0, nx).astype(np.int8)
-    j_indices = np.arange(0, ny).astype(np.int8)
+    i_indices = np.arange(0, nx).astype(np.int64)
+    j_indices = np.arange(0, ny).astype(np.int64)
     I, J = np.meshgrid(i_indices, j_indices)
 
     # Initialisation simple
     # Vent uniforme
-    U, V = 20, 20
+    U, V = 20.5, 20.5
 
     ############## Declaration des champs #############
     vx, vy = (U * np.ones((nx, ny)), V * np.ones((nx, ny)))
@@ -56,34 +56,33 @@ def main(lsettls: bool, nsiter: int, nitmp: int):
 
     # Tracer Gaussien
     T = 20
-    rsq = (xcr - 25) ** 2 + (ycr - 25) ** 2
+    rsq = (xcr - 20) ** 2 + (ycr - 20) ** 2
 
     tracer = T * np.exp(-(rsq / (2 * 10)))
     tracer_e = tracer.copy()
-    
-    
+
     ###### Plot Initial Fields #####
     fig = plot_2D_scalar_field(xcr, ycr, tracer, 25)
     plt.show()
     print(f"Step : {0}, Time : {0} s")
-    print(f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}")
+    print(
+        f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}"
+    )
     imax, jmax = np.unravel_index(np.argmax(tracer, axis=None), tracer.shape)
     print(f"Max of tracer field,  X : {xc[imax]:.2f} Y : {yc[jmax]:.2f}")
 
-
     ########### Advection ##########
     ######### Premier pas ##########
-    
+
     ######### jstep = 0 ##########
 
     # Initialisation vitesses
     vx_e, vy_e, vx, vy = sl_init(
-        vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy, vx_p=vx_p, vy_p=vy_p, LSETTLS=True
+        vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy, vx_p=vx_p, vy_p=vy_p, lsettls=lsettls
     )
 
     ######### jstep > 0 ##########
     for jstep in range(0, NITMP):
-        
         # Copie des champs
         vx, vy, tracer = backup(
             vx=vx, vy=vy, vx_e=vx_e, vy_e=vy_e, tracer=tracer, tracer_e=tracer_e
@@ -106,33 +105,36 @@ def main(lsettls: bool, nsiter: int, nitmp: int):
             dy=dy,
             nx=nx,
             ny=ny,
-            bcx_kind=bcy_kind,
+            bcx_kind=bcx_kind,
             bcy_kind=bcy_kind,
             xmin=xmin,
             xmax=xmax,
             ymin=ymin,
-            ymax=ymax
+            ymax=ymax,
         )
-        
+
         print(f"Step : {jstep}, Time : {jstep * dt} s")
-        print(f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}")
-        
+        print(
+            f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}"
+        )
+
         imax, jmax = np.unravel_index(np.argmax(tracer, axis=None), tracer.shape)
         print(f"Max of tracer field,  X : {xc[imax]:.2f} Y : {yc[jmax]:.2f}")
-        
 
     #### Print ####
     fig = plot_2D_scalar_field(xcr, ycr, tracer, 25)
     plt.show()
-    
-    print(f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}")
+
+    print(
+        f"Tracer : min {np.min(tracer)}, max {np.max(tracer)}, mean {np.mean(tracer)}"
+    )
     imax, jmax = np.unravel_index(np.argmax(tracer, axis=None), tracer.shape)
     print(f"Max of tracer field,  X : {xc[imax]:.2f} Y : {yc[jmax]:.2f}")
 
+
 if __name__ == "__main__":
-    
-    LSETTLS = True
-    NITMP = 5
-    NSITER = 4 
-    
+    LSETTLS = False
+    NITMP = 7
+    NSITER = 5
+
     main(LSETTLS, NSITER, NITMP)
