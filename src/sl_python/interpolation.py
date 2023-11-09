bcdb912334c03c5614e38c1a8a0083b5c1643a60
Loic Maurin <maurin.loic.ac@gmail.com>
2023-11-09 15:29:15 +0100

    Array syntax interpolation linear

--- src/sl_python/interpolation.py
+++ src/sl_python/interpolation.py
@@ -6,6 +6,80 @@ p1 = lambda l: (1 / 2) * l * (l + 1) * (2 - l)
 p2 = lambda l: (1 / 6) * l * (l**2 - 1)
 
 
+def interpolate_lin_2d(
+    lx: np.ndarray,
+    ly: np.ndarray,
+    psi: np.ndarray,
+    i_d: np.ndarray,
+    j_d: np.ndarray,
+    bcx_kind: int,
+    bcy_kind: int,
+    nx: int,
+    ny: int
+):
+    """Perform a 1d linear interpolation
+
+    Args:
+        lx (np.ndarray): _description_
+        ly (np.ndarray): _description_
+        psi (np.ndarray): _description_
+        i_d (np.ndarray): _description_
+        j_d (np.ndarray): _description_
+        bcx_kind (int): _description_
+        bcy_kind (int): _description_
+        nx (int): _description_
+        ny (int): _description_
+    """
+    # Polynômes de lagrange
+    p0 = lambda l: 1 - l
+    p1 = lambda l: l
+    
+    # Interp selon x -> field_hat_x
+    px = np.array([p0(lx), p1(lx)])
+    py = np.array([p0(ly), p1(ly)])
+    
+    # 1. Construire les tableaux d'indices i_d0, i_d1 / j_d0, j_d1
+    # Non periodique
+    if bcx_kind == 0: 
+        id_p1 = np.where(i_d + 1 < nx, i_d + 1, nx)
+        
+    # Periodique
+    else:
+        id_p1 = np.where(i_d + 1 < nx, i_d + 1, 0)
+    
+    # Non periodique
+    if bcy_kind == 0:
+        jd_p1 = np.where(j_d + 1 < ny, j_d + 1, ny) 
+    # Periodique
+    else:
+        jd_p1 = np.where(j_d + 1 < ny, j_d + 1, 0)
+    
+    # Lookup 
+    psi_d_i = np.zeros((4, nx, ny))
+    for i in range(nx):
+        for j in range(ny):
+            psi_d_i[0, i, j] = psi[i_d[i, j], j_d[i, j]]
+            psi_d_i[1, i, j] = psi[id_p1[i, j], j_d[i, j]]
+            
+            psi_d_i[2, i, j] = psi[i_d[i, j], jd_p1[i, j]]
+            psi_d_i[3, i, j] = psi[id_p1[i, j], jd_p1[i, j]]
+            
+    psi_d_j = np.zeros(2, (nx, ny))
+    psi_d_j[0] = px[0] * psi_d_i[0] + px[1] * psi_d_i[1]    
+    psi_d_j[1] = px[0] * psi_d_i[2] + px[1] * psi_d_i[3] 
+    
+    psi_d = py[0] * psi_d_j[0] + py[1] * psi_d_j[1]
+    
+    return psi_d
+    
+    
+    
+
+# Described as arrays
+def interpolate_cub_2d():
+    NotImplemented
+
+
 def interpolate_linear_2d(
     lx: np.float64,
     ly: np.float64,
