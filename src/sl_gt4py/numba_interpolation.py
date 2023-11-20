67ed2e1c7664aa76c4d206343ac5fc81d4cc92a8
Loic Maurin <maurin.loic.ac@gmail.com>
2023-11-20 15:21:47 +0100

    cleaning

--- src/sl_gt4py/numba_interpolation.py
+++
@@ -1,6 +0,0 @@
-import numba
-
-from sl_python.interpolation import interpolate_cub_2d, interpolate_lin_2d
-
-numba_interpolate_lin_2d = numba.jit(interpolate_lin_2d)
-numba_interpolate_cub_2d = numba.jit(interpolate_cub_2d)
