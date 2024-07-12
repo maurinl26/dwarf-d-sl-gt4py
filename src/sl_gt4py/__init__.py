7c42a53a5ab41a18bd58c06f13eec4510c9d2da9
Loïc Maurin <maurin.loic.ac@gmail.com>
2024-07-12 15:57:53 +0200

    init

---
+++ src/sl_gt4py/__init__.py
@@ -0,0 +1,8 @@
+import sl_gt4py.backup
+import sl_gt4py.blossey_velocity
+import sl_gt4py.cfl
+import sl_gt4py.copy
+import sl_gt4py.departure_search
+import sl_gt4py.floor
+import sl_gt4py.sl_2D
+import sl_gt4py.smilag_init
