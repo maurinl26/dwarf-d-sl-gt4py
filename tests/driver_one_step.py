b8fddc4e33109fa74b50138e7731b4da648fd20a
Loic Maurin <maurin.loic.ac@gmail.com>
2024-11-26 10:40:56 +0100

    tracers

---
+++ tests/driver_one_step.py
@@ -0,0 +1,104 @@
+import logging
+import numpy as np
+import sys
+
+from sl_python.plot import plot_blossey
+from sl_python.interpolation import interpolate_lin_2d
+from sl_python.sl_2D import sl_init, sl_xy
+from config import Config
+from utils.cfl import cfl_1d
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger(__name__)
+
+def init_uniform(U: float, V: float, nx: int, ny: int):
+    """Initialize horizontal velocities with uniform values (U, V)
+
+    Args:
+        U (float): Prescribed initial velocity on x
+        V (float): Prescibed initial velocity on y
+        nx (int): number of cells on x 
+        ny (int): number of cells on y
+
+    Returns:
+        Tuple[np.ndarray]: initalized velocities
+    """
+    # Vitesses
+    vx, vy = U * np.ones((nx, ny)), V * np.ones((nx, ny))
+    vx_p, vy_p = vx.copy(), vy.copy()
+    vx_e, vy_e = np.empty((nx, ny)), np.empty((nx, ny))
+
+    return vx, vy, vx_p, vy_p, vx_e, vy_e
+
+
+def backup(tracer, tracer_e):
+    tracer = tracer_e.copy()
+    return tracer
+
+
+# Driver
+def one_step_driver(
+    config: Config,
+    vx: np.ndarray,
+    vy: np.ndarray,
+    vx_e: np.ndarray,
+    vy_e: np.ndarray,
+    vx_p: np.ndarray,
+    vy_p: np.ndarray,
+    tracer: np.ndarray,
+    tracer_e: np.ndarray,
+    lsettls: bool,
+    interp_function: callable = interpolate_lin_2d
+):
+    """Driver to perform one step with prescribed velocity
+
+    Args:
+        config (Config): _description_
+        vx (np.ndarray): _description_
+        vy (np.ndarray): _description_
+        vx_e (np.ndarray): _description_
+        vy_e (np.ndarray): _description_
+        vx_p (np.ndarray): _description_
+        vy_p (np.ndarray): _description_
+        tracer (np.ndarray): _description_
+        tracer_e (np.ndarray): _description_
+        lsettls (bool): _description_
+        interp_function (callable, optional): _description_. Defaults to interpolate_lin_2d.
+
+    Returns:
+        _type_: _description_
+    """
+    # Advection
+    vx_e, vy_e, vx, vy = sl_init(
+        vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy, vx_p=vx_p, vy_p=vy_p, lsettls=lsettls
+    )
+        
+    logging.info(f"Backup")
+    logging.info(f"Tracer : min {tracer.min():.02f}, max {tracer.max():.02f}, mean {tracer.mean():.02f}")
+    logging.info(f"Tracer e : min {tracer_e.min():.02f}, max {tracer_e.max():.02f}, mean {tracer_e.mean():.02f}")
+
+    # Estimations
+    tracer_e = sl_xy(
+        config=config,
+        vx=vx,
+        vy=vy,
+        vx_e=vx_e,
+        vy_e=vy_e,
+        tracer=tracer,
+        tracer_e=tracer_e,
+        interpolation_function=interp_function,
+        nitmp=4,
+        )
+
+    # Diagnostics and outputs
+    # logging.info("Courant diagnostics")
+    # courant_xmax = np.max(cfl_1d(vx_e, config.dx, config.dt))
+    # courant_ymax = np.max(cfl_1d(vy_e, config.dy, config.dt))
+
+    # logging.info(f"Maximum courant number : {max(courant_xmax, courant_ymax):.02f}")
+    logging.info(f"Tracer (t + dt) : min {tracer_e.min():.02f}, max {tracer_e.max():.02f}, mean {tracer_e.mean():.02f}")
+
+    plot_blossey(config.xcr, config.ycr, vx, vy, tracer_e, f"./figures/one_step/one_step_end.pdf")
+    
+    return tracer_e
+
---
+++ tests/driver_uniform.py
@@ -0,0 +1,107 @@
+import logging
+import numpy as np
+import sys
+
+
+from sl_python.plot import plot_blossey
+from test_blossey import backup
+
+
+from sl_python.interpolation import interpolate_lin_2d
+from sl_python.sl_2D import sl_init, sl_xy
+from config import Config
+from utils.cfl import cfl_1d
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+def init_uniform(U: float, V: float, nx: int, ny: int):
+    """Initialization with a uniform velocity
+
+    Args:
+        U (float): prescribed velocity on x
+        V (float): prescribed velocity on y
+        nx (int): number of cells on x
+        ny (int): number of cells on y
+
+    Returns:
+        Tuple[np.ndarray]: _description_
+    """
+    # Vitesses
+    vx, vy = U * np.ones((nx, ny)), V * np.ones((nx, ny))
+    vx_p, vy_p = vx.copy(), vy.copy()
+    vx_e, vy_e = np.empty((nx, ny)), np.empty((nx, ny))
+
+    return vx, vy, vx_p, vy_p, vx_e, vy_e
+
+
+# Driver
+def sl_driver(
+    config: Config,
+    vx: np.ndarray,
+    vy: np.ndarray,
+    vx_e: np.ndarray,
+    vy_e: np.ndarray,
+    vx_p: np.ndarray,
+    vy_p: np.ndarray,
+    tracer: np.ndarray,
+    tracer_e: np.ndarray,
+    lsettls: bool,
+    nitmp: int,
+    model_endtime: float,
+    model_starttime: float,
+    T: float
+):
+    tracer_ref = tracer.copy()
+
+    # Advection
+    vx_e, vy_e, vx, vy = sl_init(
+        vx_e=vx_e, vy_e=vy_e, vx=vx, vy=vy, vx_p=vx_p, vy_p=vy_p, lsettls=lsettls
+    )
+
+    t = model_starttime
+    jstep = 0
+    while t < model_endtime:
+        jstep += 1
+        t += config.dt
+        logging.info(f"Step : {jstep}")
+        logging.info(f"Time : {100*t/model_endtime:.02f}%")
+
+        # Estimations
+        tracer_e = sl_xy(
+            config=config,
+            vx=vx,
+            vy=vy,
+            vx_e=vx_e,
+            vy_e=vy_e,
+            tracer=tracer,
+            tracer_e=tracer_e,
+            interpolation_function=interpolate_lin_2d,
+            nitmp=4,
+        )
+        
+        tracer = backup(tracer=tracer, tracer_e=tracer_e)
+
+        # Diagnostics and outputs
+        logging.debug(f"Computing Courant number with dx {config.dx} and dt {config.dt}")
+        courant_xmax = np.max(cfl_1d(vx_e, config.dx, config.dt))
+        courant_ymax = np.max(cfl_1d(vy_e, config.dy, config.dt))
+
+        logging.info(f"Maximum courant number : {max(courant_xmax, courant_ymax):.02f}")
+        logging.info(f"Tracer : min {tracer.min():.02f}, max {tracer.max():.02f}, mean {tracer.mean():.02f}")
+        
+        if t >= (T / 4) and t < (T / 4) + config.dth:
+            plot_blossey(config.xcr, config.ycr, vx, vy, tracer, f"./figures/uniform_advection/uniform_advection_{t:.02f}.pdf")
+            
+        if t >= (T / 2) and t < (T / 2) + config.dth:
+            plot_blossey(config.xcr, config.ycr, vx, vy, tracer, f"./figures/uniform_advection/uniform_advection_{t:.02f}.pdf")
+            
+        if t >= (3 * T / 4) and t < (3 * T / 4) + config.dth:
+            plot_blossey(config.xcr, config.ycr, vx, vy, tracer, f"./figures/uniform_advection/uniform_advection_{t:.02f}.pdf")
+
+    e_inf = np.max(np.abs(tracer - tracer_ref))
+    e_2 = np.sqrt((1 / (config.nx * config.ny)) * np.sum((tracer - tracer_ref) ** 2))
+
+    logging.info(f"Error E_inf : {e_inf}")
+    logging.info(f"Error E_2 : {e_2}")
+
+    plot_blossey(config.xcr, config.ycr, vx, vy, tracer, f"./figures/uniform_advection/uniform_advection_{t:.02f}.pdf")   
+    
